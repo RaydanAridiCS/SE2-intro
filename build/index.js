404d2f721dad66f00e350ff40ed761eec3f60993
RaydanAridiCS <RaydanAridi.CS@gmail.com>
2025-04-17 22:17:26 +0300

    Initialized project environment

---
+++ build/index.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const config_1 = __importDefault(require("./config"));
+const firstName = "Raydan";
+console.log("Hello from TypeScript!");
+console.log("My name is ", firstName);
+console.log('Hello, world!, the secret is ', config_1.default.SECRET);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import config from './config';
+
+const firstName= "Raydan";
+
+console.log("Hello from TypeScript!");
+console.log("My name is ", firstName);
+
+
+console.log('Hello, world!, the secret is ', config.SECRET);
