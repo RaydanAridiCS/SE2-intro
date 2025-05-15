8c5b485976f287352946e5ee49b0f81f520d2bfd
RaydanAridiCS <RaydanAridi.CS@gmail.com>
2025-05-15 15:32:57 +0300

    Assignment 4.2: Added the mapper for the XML file

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
 import logger from "./util/logger";
-import { CsvParser } from "./parsers/csvParser";
-import { CsvCakeMapper } from "./mappers/Cake.mapper";
+// import { CsvParser } from "./parsers/csvParser";
+// import { CsvCakeMapper } from "./mappers/Cake.mapper";
 import { CsvOrderMapper } from "./mappers/Order.mapper";
+
+import { XmlParser } from "./parsers/xmlParser";
+import { XmlToyMapper } from "./mappers/Toy.mapper";
+// import { XmlOrderMapper } from "./mappers/Order.mapper";
+
 
 async function main() {
 
-    const data = await CsvParser.readCsv('src/data/cake orders.csv', true);
-    const cakeMapper = new CsvCakeMapper();
-    const orderMapper = new CsvOrderMapper(cakeMapper);
-    const orders = data.map(row => orderMapper.map(row));
+    // const data = await CsvParser.readCsv('src/data/cake orders.csv', true);
+    // const cakeMapper = new CsvCakeMapper();
+    // const orderMapper = new CsvOrderMapper(cakeMapper);
+    // const orders = data.map(row => orderMapper.map(row));
+
+    // orders.forEach(order => {
+    //     logger.info("%o", order);
+    // });
+
+    const toyData = await XmlParser.readXml('src/data/toy orders.xml');
+    const toyMapper = new XmlToyMapper();
+    const orderMapper = new CsvOrderMapper(toyMapper);
+    const orders = toyData.map(row => orderMapper.map(row));
 
     orders.forEach(order => {
         logger.info("%o", order);
     });
-
 
 
 }
